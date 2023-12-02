321c0bace8cd36790fee21d07b3d2d8fb3491eea
yandv <allanmxr@gmail.com>
2023-12-01 21:56:38 -0300

    chore: changed the backend server port to 8080

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("/api/v1");
+
+  app.setGlobalPrefix('/api/v1');
+
   await app.listen(8080);
 }
+
 bootstrap();
