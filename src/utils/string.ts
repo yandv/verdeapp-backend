539c38c1f8eeace45ce3ae3538393ed63845407f
yandv <allanmxr@gmail.com>
2023-12-03 02:14:30 -0300

    fix: resolved a problem that parseAuth doesnt handle undefined authorizations

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,4 +1,5 @@
 export function parseAuth(authorization?: string): string | undefined {
+    if (!authorization) return undefined;
     const [type, token] = authorization.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
 }
